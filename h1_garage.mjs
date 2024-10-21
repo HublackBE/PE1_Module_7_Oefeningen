a51a3117168281ad77343a2834a6fc462cb82f14
Hublack <71221641+HublackBE@users.noreply.github.com>
2024-10-21 10:12:43 +0200

    Remove unnecessary import of parse

--- h1_garage.mjs
+++ h1_garage.mjs
@@ -1,6 +1,5 @@
 import * as readline from 'node:readline/promises';
 import {stdin as input, stdout as output} from 'node:process';
-import { parse } from 'node:path';
 const userInput = readline.createInterface(input, output);
 
 
